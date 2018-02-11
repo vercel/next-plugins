0242e12c4d38d16ef82edfcaa91515697ff185cf
Tim Neutkens <timneutkens@me.com>
2018-02-11 21:52:54 +0100

    Add next-mdx

---
+++ packages/next-mdx/index.js
@@ -0,0 +1,22 @@
+module.exports = (nextConfig = {}) => {
+  return Object.assign({}, nextConfig, {
+    webpack(config, options) {
+      if (!options.defaultLoaders) {
+        throw new Error(
+          'This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade'
+        )
+      }
+
+      config.module.rules.push({
+        test: /\.mdx?$/,
+        use: [options.defaultLoaders.babel, 'mdx-loader']
+      })
+
+      if (typeof nextConfig.webpack === 'function') {
+        return nextConfig.webpack(config, options)
+      }
+
+      return config
+    }
+  })
+}
