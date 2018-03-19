d44e80424cd27e04310151f1e482cb698cfea2b7
ashwinibm <ashwini@baysidetech.in>
2018-03-19 16:53:38 +0530

    Allow for postcss-loader options to be passed

--- packages/next-css/index.js
+++ packages/next-css/index.js
@@ -12,7 +12,7 @@ module.exports = (nextConfig = {}) => {
       }
 
       const { dev, isServer } = options
-      const { cssModules, cssLoaderOptions } = nextConfig
+      const { cssModules, cssLoaderOptions, postcssLoaderOptions } = nextConfig
       // Support the user providing their own instance of ExtractTextPlugin.
       // If extractCSSPlugin is not defined we pass the same instance of ExtractTextPlugin to all css related modules
       // So that they compile to the same file in production
@@ -33,6 +33,7 @@ module.exports = (nextConfig = {}) => {
       options.defaultLoaders.css = cssLoaderConfig(config, extractCSSPlugin, {
         cssModules,
         cssLoaderOptions,
+        postcssLoaderOptions,
         dev,
         isServer
       })
