5ec7d7e6caea7d80e7866cf20668b480035424bd
Victor Denisov <mr.plag@gmail.com>
2018-09-26 15:21:07 +0300

    [next-source-maps] Ability to change source-map type (#255)
    * [next-source-maps] Ability to change source-map type
    
    * New way to pass configuration options into plugin

--- packages/next-source-maps/index.js
+++ packages/next-source-maps/index.js
@@ -1,29 +1,39 @@
-module.exports = (nextConfig = {}) => {
-  return Object.assign({}, nextConfig, {
-    webpack(config, options) {
-      if (!options.defaultLoaders) {
-        throw new Error(
-          'This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade'
-        )
-      }
+module.exports = (options = {}) => {
+  const configurator = (nextConfig = {}) => {
+    return Object.assign({}, nextConfig, {
+      webpack(config, options) {
+        if (!options.defaultLoaders) {
+          throw new Error(
+            'This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade'
+          )
+        }
 
-      const { dev } = options
+        const { dev } = options
 
-      if (!dev) {
-        config.devtool = 'source-map'
+        if (!dev) {
+          config.devtool = options.devtool || 'source-map';
 
-        for (const plugin of config.plugins) {
-          if (plugin.constructor.name === 'UglifyJsPlugin') {
-            plugin.options.sourceMap = true
-            break
+          for (const plugin of config.plugins) {
+            if (plugin.constructor.name === 'UglifyJsPlugin') {
+              plugin.options.sourceMap = true;
+              break;
+            }
           }
         }
-      }
 
-      if (typeof nextConfig.webpack === 'function') {
-        return nextConfig.webpack(config, options)
+        if (typeof nextConfig.webpack === 'function') {
+          return nextConfig.webpack(config, options)
+        }
+        return config
       }
-      return config
-    }
-  })
+    })
+  }
+
+  if (options.devtool) {
+    return configurator
+  }
+
+  console.warn('Use next-source-maps plugin without configuration is deprecated. You should pass configuration object as argument and get function for use for compatibility with newer versions.')
+
+  return configurator(options)
 }
--- packages/next-source-maps/readme.md
+++ packages/next-source-maps/readme.md
@@ -20,7 +20,7 @@ Create a next.config.js
 
 ```js
 // next.config.js
-const withSourceMaps = require('@zeit/next-source-maps')
+const withSourceMaps = require('@zeit/next-source-maps')()
 module.exports = withSourceMaps({
   webpack(config, options) {
     return config
@@ -32,4 +32,22 @@ Then you can run a regular build command and source maps will be outputted for t
 
 ```bash
 npm run build
+```
+
+### Configuring plugin
+By default devtool is `source-map`. If you want use different devtool type, you can pass it with options.
+
+```js
+// next.config.js
+const withSourceMaps = require('@zeit/next-source-maps')({
+  devtool: 'hidden-source-map'
+})
+
+module.exports = withSourceMaps(
+  {
+    webpack(config, options) {
+      return config
+    }
+  }
+)
 ```
