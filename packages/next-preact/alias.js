071946e5957d95454bfe36bffd81203254a5d339
Tim Neutkens <tim@timneutkens.nl>
2019-02-15 00:04:44 +0900

    Update packages/next-preact/alias.js
    review modification
    
    Co-Authored-By: toshi1127 <32378535+toshi1127@users.noreply.github.com>

--- packages/next-preact/alias.js
+++ packages/next-preact/alias.js
@@ -2,6 +2,6 @@ const path = require('path')
 const moduleAlias = require('module-alias')
 
 module.exports = () => {
-  moduleAlias.addAlias('react', path.resolve(__dirname, './preact-compat.js'))
+  moduleAlias.addAlias('react', path.join(__dirname, './preact-compat.js'))
   moduleAlias.addAlias('react-dom', 'preact-compat')
 }
