d1fcc8758d4402b9968d93a81eadb0ec8deb1325
Ovyerus <iamovyerus@gmail.com>
2019-04-16 14:48:31 +1000

    fix(next-css): remove `ignore-loader` block in css-loader-config
    This allows CSS to be transpiled on the server even if not using CSS modules, which allows stuff like `:export` blocks from ICSS to work.

--- packages/next-css/css-loader-config.js
+++ packages/next-css/css-loader-config.js
@@ -97,11 +97,6 @@ module.exports = (
     )
   }
 
-  // When not using css modules we don't transpile on the server
-  if (isServer && !cssLoader.options.modules) {
-    return ['ignore-loader']
-  }
-
   // When on the server and using css modules we transpile the css
   if (isServer && cssLoader.options.modules) {
     return [cssLoader, postcssLoader, ...loaders].filter(Boolean)
