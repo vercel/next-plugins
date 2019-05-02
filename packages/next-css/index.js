08e36d9b74cc9f196ac22d764023e93064592519
vabhishek-me <abhishek.k.v76076@gmail.com>
2019-05-02 17:03:10 +0530

    added parameters moduleExtension, moduleLoaderOptions to support .module extension

--- packages/next-css/index.js
+++ packages/next-css/index.js
@@ -10,11 +10,11 @@ module.exports = (nextConfig = {}) => {
       }
 
       const { dev, isServer } = options
-      const { cssModules, cssLoaderOptions, postcssLoaderOptions } = nextConfig
+      const { cssModules, cssLoaderOptions, postcssLoaderOptions, moduleExtension, moduleLoaderOptions } = nextConfig
 
       options.defaultLoaders.css = cssLoaderConfig(config, {
         extensions: ['css'],
-        cssModules,
+        cssModules: moduleExtension ? false : cssModules,
         cssLoaderOptions,
         postcssLoaderOptions,
         dev,
@@ -31,8 +31,34 @@ module.exports = (nextConfig = {}) => {
           }
           return true
         },
+        exclude: moduleExtension ? /\.module\.css$/ : /^$/,
         use: options.defaultLoaders.css
       })
+
+      if (moduleExtension) {
+        config.module.rules.push({
+          test: /\.module\.css$/,
+          issuer(issuer) {
+            if (issuer.match(/pages[\\/]_document\.js$/)) {
+              throw new Error(
+                'You can not import CSS files in pages/_document.js, use pages/_app.js instead.'
+              )
+            }
+            return true
+          },
+          use: cssLoaderConfig(config, {
+            extensions: ['css'],
+            cssModules: true,
+            cssLoaderOptions: moduleLoaderOptions || {
+              importLoaders: 1,
+              localIdentName: '[local]___[hash:base64:8]'
+            },
+            postcssLoaderOptions,
+            dev,
+            isServer
+          })
+        });
+      }
 
       if (typeof nextConfig.webpack === 'function') {
         return nextConfig.webpack(config, options)
