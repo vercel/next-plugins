ef6f626176d2b886bacba16391766b4a64f95073
Tim Neutkens <timneutkens@me.com>
2020-12-17 15:50:17 +0100

    Deprecate / remove packages that now have built-in features
    Deprecates all packages that are built-in Next.js features or had significantly low usage.

--- packages/next-preact/alias.js
+++
@@ -1,7 +0,0 @@
-const moduleAlias = require('module-alias')
-
-module.exports = () => {
-  moduleAlias.addAlias('react', 'preact/compat')
-  moduleAlias.addAlias('react-dom', 'preact/compat')
-  moduleAlias.addAlias('react-ssr-prepass', 'preact-ssr-prepass')
-}
